8ee9b80d6c5c246bd68c965bcd1a56f95d0fb9ea
Allan Anousinh <thegreatallandini@gmail.com>
2018-10-05 14:08:03 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,89 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-class App extends Component {
+class App extends React.Component {
 
-constructor(props) {
-
-  super(props);
+constructor() {
+  super();
   
     this.state = {
-
-      // term: '',
-
-      img: '',
-
+      jokes1: '',
       jokes: '',
 
-      
-    };
-
-  }
-
-  // onChange = (event) => {
-  //     this.setState({term: event.target.value}); }
-  // const api_key = 'dc6zaTOxFJmzC';
-  // const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`;
-
-  // const url = `https://cat-fact.herokuapp.com/facts`
+    };}
 
   handleSubmit = (event) => {
     event.preventDefault();
 
-    const api_key = 'ee041990-3572-4441-a138-65b21f60b6f5';
-    const url = `https://api.thecatapi.com/v1/images/search?${this.state.term}&api_key=${api_key}`;
+    axios.get(`https://geek-jokes.sameerkumar.website/api`)
+    .then(res => {
+      const jokes = res.data;
+      console.log(jokes);
 
-      fetch(url)
-        .then(response => response.json())
-
-        .then(data => this.setState({term:'', img: data[0].url}))
-        
-
-        .catch(e => console.log('error', e));
-      
-      
     axios.get(`https://api.chucknorris.io/jokes/random`)
     .then(res => {
-      const jokes = res.data.value;
-      this.setState({ jokes: jokes });
-      console.log(jokes);
-    })
-      }
-
-
+      const jokes1 = res.data.value;
+      
+      this.setState({
+         jokes1: jokes1,
+         jokes: jokes
+        });
+      console.log(jokes1);
+    })  
+   })
+    }
 
       render() {
         return (
-          
+
           <div className="App">
-
         <header className="App-header">
-
           <h1 className="title">*HaCkAtHon</h1>
-     
         </header>
-
       <form>
-          {/* <input value={this.state.term} onChange={this.onChange} /> */}
-
-          <button onClick={this.handleSubmit}>refresh</button>
+          <button class="btn btn-primary" onClick={this.handleSubmit}>Refresh</button>
       </form>
 
-          <img src={this.state.img} height = "700" alt={this.state.img} />
+<div>
 
 
-<p>
-
-  <li>{this.state.jokes}</li>
-
-</p>
-
-
-         
-      </div>
+  <p>{this.state.jokes}</p>  
+   <p>{this.state.jokes1}</p>
    
-   );
+</div>
 
+      </div>
+   );
   }
 }
 export default App;
